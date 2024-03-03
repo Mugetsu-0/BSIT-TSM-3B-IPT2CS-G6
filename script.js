82540ff6fa018abca62f205013a04d2056884ac4
Christian <ramilxtian@gmail.com>
2024-03-03 20:19:10 +0800

    1c

---
+++ script.js
@@ -0,0 +1,5 @@
+function showName(img) {
+    console.log("Function called");
+    var figcaption = image.nextElementSibling; // Get the next element, which is the figcaption
+    figcaption.style.opacity = "1"; // Set the opacity to 1 to show the figcaption
+}
